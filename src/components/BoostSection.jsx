c631ffad648ed26088d745c986bc1d7ab4db2aa0
Austin Nze <austinnze1000@gmail.com>
2023-10-19 09:32:55 +0100

    first feature branch code

--- src/components/BoostSection.jsx
+++ src/components/BoostSection.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const BoostSection = () => {
   return (
-      <section className='min-h-[30vh] 
-    \'>
-      
+    <section className='min-h-[30vh] 
+    '>
+      <h2>Boost your links today</h2>
     </section>
   )
 }
 
 export default BoostSection
+
+// I created a new branch and I'm trying to do some cool stuffs with VCs
