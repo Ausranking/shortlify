09a144df4b1697e0baa4c2e283caa48a59b18344
Austin Nze <austinnze1000@gmail.com>
2023-10-25 03:30:40 +0100

    final touches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,11 +27,17 @@ const Footer = () => {
           </div>
         ))}
       </div>
-      <div className="flex gap-5 text-emerald-500">
-        <FaFacebook size={25} className="hover:text-cyan" />
-        <FaTwitter size={25} className="hover:text-cyan" />
-        <FaPinterest size={25} className="hover:text-cyan" />
-        <FaTwitter size={25} className="hover:text-cyan" />
+      <div className="flex gap-5 text-cyan">
+        <div>
+            <FaFacebook
+          size={25}
+          className="hover:text-dark_violet hover:scale-105"
+        />
+        </div>
+      
+        <FaTwitter size={25} className="hover:text-dark_violet" />
+        <FaPinterest size={25} className="hover:text-dark_violet" />
+        <FaTwitter size={25} className="hover:text-dark_violet" />
       </div>
     </footer>
   );
--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,18 +1,19 @@
 import StatCard from "./StatCard";
-import {icon1,icon2, icon3} from '/src/assets/images'
+import { icon1, icon2, icon3 } from "/src/assets/images";
 const Stat = () => {
   return (
-    <section className=" bg-gray w-11/12 m-auto min-h-fit pt-1 pb-10">
+    <section className="w-11/12 m-auto min-h-fit pt-1 pb-10 lg:pb-32">
       <div className="text-center">
-        <h2 className="text-2xl text-darker_violet font-bold py-1">
+        <h2 className="text-2xl lg:text-3xl text-darker_violet font-bold py-1">
           Advanced Statistics
         </h2>
-        <p className="text-lg p-2 md:p-3 lg:text-2xl lg:p-5 line-clamp-2 w-11/12">
-          Track how your links are performing accross the web with our advanced
-          statistics dashboard.
+        <p className=" text-xl p-2 text-slate-600">
+          Track how your links are performing <br /> accross the web with our
+          advanced statistics dashboard.
         </p>
       </div>
-      <section className="mt-16 grid md:grid-cols-2 lg:grid-cols-3 max-sm:gap-10 gap-5">
+      <section className="relative mt-16 grid md:grid-cols-3 max-sm:gap-16 gap-5">
+        <div className="md:h-2 w-full absolute top-1/2 bg-cyan"></div>
         <div>
           <StatCard
             icon={icon1}
@@ -20,19 +21,18 @@ const Stat = () => {
             desc="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content"
           />
         </div>
-        {/* <div className="bg-cyan h-2 w-full "></div> */}
-        <div className="lg:translate-y-8">
+        <div className="md:translate-y-8 relative">
           <StatCard
-            icon={icon1}
-            title="Brand Recognition"
-            desc="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content"
+            icon={icon2}
+            title="Detailed Records"
+            desc="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
           />
         </div>
-        <div className="lg:translate-y-16">
+        <div className="md:translate-y-16">
           <StatCard
-            icon={icon1}
-            title="Brand Recognition"
-            desc="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content"
+            icon={icon3}
+            title="Fully Customizable"
+            desc="Improve brand awareness and content discoverability through customizable links. superchanging audience engagement."
           />
         </div>
       </section>
