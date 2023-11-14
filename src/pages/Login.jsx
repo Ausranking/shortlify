02d7ad5f1b6c901a07edba26e8b19a435496bec0
Austin Nze <austinnze1000@gmail.com>
2023-11-14 02:11:38 +0100

    implement login page (#11)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,55 @@
 //Create a login page for shortlify in another branch, pull request to features before main
+
+import { useState } from "react";
+import Logo from "../components/Logo";
+
+const Login = () => {
+  return (
+    <section className=" flex max-lg:gap-20 md:gap-10 max-sm:gap-1 flex-col py-10 md:py-5 max-lg:items-center px-4 md:w-11/12 m-auto">
+      <div>
+        <Logo />
+      </div>
+
+      <form className="w-full py-10 m-auto md:max-w-lg">
+        <div className="text-center p-2">
+          <h1 className="font-bold text-xl p-5">Welcome back</h1>
+          <p className="text-lg p-3">Please enter your details to login</p>
+        </div>
+        <label className="">
+          Email address <br />
+          <input
+            className="w-full "
+            type="email"
+            placeholder="Enter your email address"
+          />
+        </label>
+
+        <label>
+          <br />
+          Password <br />
+          <input type="password" placeholder="Enter your password" />
+        </label>
+        <p className="pt-10 pb-3 text-right text-sm text-darker-violet">
+          Forgot passsword?
+        </p>
+        <button
+          type="submit"
+          className="border w-full bg-gradient-to-tr from-rose to-dark-violet text-gray p-2"
+        >
+          Continue
+        </button>
+        <p className="text-sm py-3 text-center">
+          Don't have an account?
+          <span className="text-rose py-10 cursor-pointer">
+            Create account
+          </span>{" "}
+        </p>
+        <p className="text-center text-sm mt-5 sm:mt-20">
+          Terms and privacy policy
+        </p>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
